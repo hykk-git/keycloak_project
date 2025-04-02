72414bff308aaea1d3d451db8fede0179767af9d
hykk-git <gratia1206@naver.com>
2025-04-02 01:10:12 +0000

    feat: mozilla 연동

--- django-jwt/board/urls.py
+++ django-jwt/board/urls.py
@@ -3,10 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', main_view, name='main'),
-    path("google/login/", google_login, name="google_login"),
     path("login/status/", login_status, name="login_status"),
     path("logout/", logout_view, name="logout"),
     path('board/', board_view, name='board'),
-    path("login/oauth2/google/", google_callback, name="google_callback"),
     path("login/keycloak/callback/", keycloak_callback, name="keycloak_callback"),
 ]
--- django-jwt/config/urls.py
+++ django-jwt/config/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('board.urls')), 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  
+    path('oidc/', include('mozilla_django_oidc.urls')),
 ]
